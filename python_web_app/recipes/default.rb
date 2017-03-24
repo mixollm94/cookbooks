a6cf8739df17cc039c803bd63bb346e2c822ce5b
Mykhailo.Smolynets <mixollm94@gmail.com>
2017-03-24 14:13:24 +0200

    python web app commit 2(change default.rb file)

--- python_web_app/recipes/default.rb
+++ python_web_app/recipes/default.rb
@@ -6,3 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "python_web_app::nginx"
+include_recipe "python_web_app::pip"
+include_recipe "python_web_app::virtualenv"
+
+directory '/home/ec2-user/myproject' do
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode "0666"
+  action :create
+end
+
+template "/home/ec2-user/myproject/myproject.py" do
+  source "myproject.py"
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode "0666"
+end
+
