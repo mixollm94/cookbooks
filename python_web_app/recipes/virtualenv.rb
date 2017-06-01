77beaf44b16d00ffc13166a94476bf4cbf632303
Mykhailo.Smolynets <mixollm94@gmail.com>
2017-06-01 15:10:06 +0300

    commit with latest virtualenv.rb

--- python_web_app/recipes/virtualenv.rb
+++ python_web_app/recipes/virtualenv.rb
@@ -3,3 +3,10 @@ include_recipe "python_web_app::pip"
 python_pip "virtualenv" do
   action :install
 end
+
+python_virtualenv "/home/ec2-user/myproject/myprojectenv" do
+  owner "ec2-user"
+  group "ec2-user"
+  options "--system-site-packages"
+  action :create
+end
